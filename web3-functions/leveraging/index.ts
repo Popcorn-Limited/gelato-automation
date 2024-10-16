5ac8deb65a7b0c3b46c8c5f8ac32df2a2e7dc74f
RedVeil <leon.niesler@gmail.com>
2024-10-16 15:56:55 +0200

    wip - added more checks to the leverage bot

--- web3-functions/leveraging/index.ts
+++ web3-functions/leveraging/index.ts
@@ -3,25 +3,43 @@ import {
   Web3FunctionContext,
 } from "@gelatonetwork/web3-functions-sdk";
 import { Address, Chain, createPublicClient, encodeFunctionData, getAddress, http, PublicClient, zeroAddress } from 'viem'
-import { arbitrum, mainnet, optimism, polygon } from "viem/chains";
+import {
+  arbitrum,
+  avalanche,
+  base,
+  bsc,
+  fraxtal,
+  mainnet,
+  optimism,
+  polygon,
+  xLayer,
+} from "viem/chains";
 
-export const NetworkByChainId: { [key: number]: Chain } = {
-  [mainnet.id]: mainnet,
-  [polygon.id]: polygon,
-  [optimism.id]: optimism,
-  [arbitrum.id]: arbitrum,
+export const ChainById: { [key: number]: Chain } = {
+  1: mainnet,
+  10: optimism,
+  42161: arbitrum,
+  137: polygon,
+  56: bsc,
+  196: xLayer,
+  8453: base,
+  252: fraxtal,
+  43114: avalanche
 }
 
-const RPC_URLS: { [key: number]: string } = {
-  [mainnet.id]: "https://eth-mainnet.alchemyapi.io/v2/",
-  [polygon.id]: "https://polygon-mainnet.g.alchemy.com/v2/",
-  [arbitrum.id]: "https://arb-mainnet.alchemyapi.io/v2/",
-  [optimism.id]: "https://opt-mainnet.alchemyapi.io/v2/"
-}
+export const RPC_URLS: { [key: number]: string } = {
+  [1]: `https://eth-mainnet.alchemyapi.io/v2/`,
+  [10]: `https://opt-mainnet.g.alchemy.com/v2/`,
+  [42161]: `https://arb-mainnet.g.alchemy.com/v2/`,
+  [137]: `https://polygon-mainnet.g.alchemy.com/v2/`,
+  [56]: `https://bnb-mainnet.g.alchemy.com/v2/`,
+  [196]: "https://rpc.xlayer.tech",
+  [8453]: `https://base-mainnet.g.alchemy.com/v2/`,
+  [252]: `https://frax-mainnet.g.alchemy.com/v2/`,
+  [43114]: `https://avax-mainnet.g.alchemy.com/v2/`
+};
 
-const LEGACY_CHAINS: number[] = [polygon.id]
-
-async function verifyLTV(client: PublicClient, strategy: Address): Promise<boolean> {
+async function fetchLTV(client: PublicClient, strategy: Address): Promise<{ max: bigint, target: bigint, current: bigint }> {
   const results = await client.multicall({
     contracts: [
       {
@@ -43,55 +61,102 @@ async function verifyLTV(client: PublicClient, strategy: Address): Promise<boole
   });
   console.log("fetched ltv")
 
-  const maxLTV = results[0].result ?? parseInt("0");
-  const targetLTV = results[1].result ?? parseInt("0");
-  const currentLTV = results[2].result ?? parseInt("0");
+  const maxLTV = results[0].result ?? BigInt("0");
+  const targetLTV = results[1].result ?? BigInt("0");
+  const currentLTV = results[2].result ?? BigInt("0");
   console.log({ maxLTV, targetLTV, currentLTV })
 
-  // ltv on track
-  if ((targetLTV <= currentLTV) && (currentLTV < maxLTV))
-    return true;
-
-  return false;
+  return { max: maxLTV, target: targetLTV, current: currentLTV }
 }
 
+
 Web3Function.onRun(async (context: Web3FunctionContext) => {
-  console.log("LEVERAGE 5X ETHX")
   const alchemyKey = await context.secrets.get("ALCHEMY_KEY");
   const strategy = getAddress(await context.secrets.get("STRATEGY") || zeroAddress);
+  // const pool = getAddress(await context.secrets.get("POOL") || zeroAddress);
+  // const asset = getAddress(await context.secrets.get("ASSET") || zeroAddress);
+
+  const minLTV = BigInt(await context.secrets.get("MIN_LTV") || "0");
+  const maxLTV = BigInt(await context.secrets.get("MAX_LTV") || "0");
+  const maxInterestRate = BigInt(await context.secrets.get("MAX_INTEREST_RATE") || "0");
 
   // Should be the fixed gelato address for this task
   const maxGas = BigInt(await context.secrets.get("MAX_GAS") || "0");
+  // comma seperated chainIds
+  const legacyChainNumbers = await context.secrets.get("LEGACY_CHAINS");
 
-  console.log({ alchemyKey, strategy, maxGas })
+  console.log({ strategy, minLTV, maxLTV, maxInterestRate, maxGas, legacyChainNumbers })
 
-  // Check env variables set
-  if (strategy === zeroAddress || maxGas === BigInt("0")) {
+  // Check env variables set for typing
+  if (strategy === zeroAddress || !legacyChainNumbers) {
     console.log("ERROR: ENV")
     return {
       canExec: false,
       callData: []
     }
   }
+  const legacyChains = legacyChainNumbers.split(",").map(value => Number(value))
 
   // Initiate Client
+  console.log("INITIATING CLIENT")
   const { multiChainProvider } = context;
   const chainId = multiChainProvider.default()._network.chainId;
   const client = createPublicClient({
-    chain: NetworkByChainId[chainId],
+    chain: ChainById[chainId],
     transport: http(`${RPC_URLS[chainId]}${alchemyKey}`),
     batch: {
       multicall: true,
     }
   });
-  console.log("Set up basics")
 
+  /// @dev No current way to lever the strategy down / up besides calling adjustLeverage
+  // console.log("FETCHING INTEREST RATE")
+  // const reserveData = await client.readContract({
+  //   address: pool,
+  //   abi: PoolAbi,
+  //   functionName: "getReserveData",
+  //   args: [asset]
+  // })
+
+  // if (reserveData[4] > maxInterestRate) {
+  //   console.log("EXECUTING: currentInterestRate > maxInterestRate")
+  //   return {
+  //     canExec: true,
+  //     callData: [
+  //       {
+  //         to: strategy,
+  //         data: encodeFunctionData({
+  //           abi: strategyAbi,
+  //           functionName: 'adjustLeverage',
+  //         }),
+  //       },
+  //     ],
+  //   };
+  // }
+
+  console.log("Fetching LTV")
   // verify trigger conditions
-  const goodLTV = await verifyLTV(client, strategy);
+  const { max, target, current } = await fetchLTV(client, strategy);
+
+  if (current > maxLTV) {
+    console.log("EXECUTING: current > maxLTV")
+    return {
+      canExec: true,
+      callData: [
+        {
+          to: strategy,
+          data: encodeFunctionData({
+            abi: strategyAbi,
+            functionName: 'adjustLeverage',
+          }),
+        },
+      ],
+    };
+  }
 
   // no need to adjust
-  if (goodLTV) {
-    console.log("ERROR: LTV")
+  if ((target <= current) && (current < max)) {
+    console.log("ERROR: HEALTHY LTV")
     return {
       canExec: false,
       callData: [],
@@ -99,8 +164,9 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
   }
 
   // Check Gas Threshold
+  console.log("CHECKING GAS THRESHOLD")
   let estimatedGasPrice = BigInt(0)
-  if (LEGACY_CHAINS.includes(chainId)) {
+  if (legacyChains.includes(chainId)) {
     const { gasPrice } = await client.estimateFeesPerGas({ type: "legacy" })
     estimatedGasPrice = await client.estimateContractGas({
       address: strategy,
@@ -118,7 +184,7 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
       maxPriorityFeePerGas
     })
   }
-  console.log("got gas price: ", estimatedGasPrice)
+  console.log("GAS PRICE: ", estimatedGasPrice)
 
   if (estimatedGasPrice > maxGas) {
     console.log("ERROR: maxGas")
@@ -128,7 +194,7 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     }
   }
 
-  console.log("run")
+  console.log("EXECUTING")
   // Return execution call data
   return {
     canExec: true,
@@ -151,3 +217,6 @@ const strategyAbi = [
   { "inputs": [], "name": "maxLTV", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" },
   { "inputs": [], "name": "targetLTV", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }
 ] as const
+
+
+const PoolAbi = [{ "inputs": [{ "internalType": "address", "name": "asset", "type": "address" }], "name": "getReserveData", "outputs": [{ "components": [{ "components": [{ "internalType": "uint256", "name": "data", "type": "uint256" }], "internalType": "struct DataTypes.ReserveConfigurationMap", "name": "configuration", "type": "tuple" }, { "internalType": "uint128", "name": "liquidityIndex", "type": "uint128" }, { "internalType": "uint128", "name": "currentLiquidityRate", "type": "uint128" }, { "internalType": "uint128", "name": "variableBorrowIndex", "type": "uint128" }, { "internalType": "uint128", "name": "currentVariableBorrowRate", "type": "uint128" }, { "internalType": "uint128", "name": "currentStableBorrowRate", "type": "uint128" }, { "internalType": "uint40", "name": "lastUpdateTimestamp", "type": "uint40" }, { "internalType": "uint16", "name": "id", "type": "uint16" }, { "internalType": "address", "name": "aTokenAddress", "type": "address" }, { "internalType": "address", "name": "stableDebtTokenAddress", "type": "address" }, { "internalType": "address", "name": "variableDebtTokenAddress", "type": "address" }, { "internalType": "address", "name": "interestRateStrategyAddress", "type": "address" }, { "internalType": "uint128", "name": "accruedToTreasury", "type": "uint128" }, { "internalType": "uint128", "name": "unbacked", "type": "uint128" }, { "internalType": "uint128", "name": "isolationModeTotalDebt", "type": "uint128" }], "internalType": "struct DataTypes.ReserveDataLegacy", "name": "", "type": "tuple" }], "stateMutability": "view", "type": "function" }] as const
